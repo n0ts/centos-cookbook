ca0f369be0bb15b12bfb9418014dc0091d63b8fc
n0ts <me@n0ts.org>
2014-06-28 23:22:47 +0900

    Fix name metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'centos'
-maintainer       "Naoya Nakazawa"
-maintainer_email "me@n0ts.org"
-license          "All rights reserved"
-description      "Installs/Configures centos"
+maintainer       'Naoya Nakazawa'
+maintainer_email 'me@n0ts.org'
+license          'All rights reserved'
+description      'Installs/Configures centos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          '0.1.0'
 
 %w{ yum }.each do |depend|
   depends depend
